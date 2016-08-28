33587fa46ce09872ee303ba9860738313b8f5d72
yohei <yohei@e-ml.mobi>
2016-08-28 14:49:04 +0000

    kadai 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,14 @@ class ItemsController < ApplicationController
   before_action :logged_in_user , except: [:show]
   before_action :set_item, only: [:show]
 
+
   def new
     if params[:q]
       response = RakutenWebService::Ichiba::Item.search(
         keyword: params[:q],
         imageFlag: 1,
       )
+   
       @items = response.first(20)
     end
   end
